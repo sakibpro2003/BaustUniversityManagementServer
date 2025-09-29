c375da79e7654626f173cd98555df216da536a5a
sakibpro2003 <sakibprodhan2003@gmail.com>
2025-09-29 10:45:18 +0600

    feat: catchAsync Higher Order Function created

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,33 +1,26 @@
-import { NextFunction, Request, Response } from 'express';
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { StudentServices } from './student.service';
 import sendResponse from '../../utils/sendResponse';
 import status from 'http-status';
+import catchAsync from '../../utils/catchAsync';
 
-const getAllStudents = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const result = await StudentServices.getAllStudentsFromDB();
 
-    sendResponse(res, {
-      statusCode: status.OK,
-      success: true,
-      message: 'Student retrieved successfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
 
-const getSingleStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
+const getAllStudents = catchAsync(async (req, res, next) => {
+  const result = await StudentServices.getAllStudentsFromDB();
+
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: 'Student retrieved successfully',
+    data: result,
+  });
+});
+
+const getSingleStudent = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const { studentId } = req.params;
 
     const result = await StudentServices.getSingleStudentFromDB(studentId);
@@ -38,10 +31,8 @@ const getSingleStudent = async (
       message: 'Student is retrieved succesfully',
       data: result,
     });
-  } catch (err) {
-    next(err);
-  }
-};
+  },
+);
 
 export const StudentControllers = {
   getAllStudents,
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,25 +1,23 @@
-import { RequestHandler } from 'express';
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { StudentServices } from './user.service';
 import sendResponse from '../../utils/sendResponse';
 import status from 'http-status';
-const createStudent: RequestHandler = async (req, res, next) => {
-  try {
-    const { student: studentData, password } = req.body;
-    const result = await StudentServices.createStudentIntoDB(
-      password,
-      studentData,
-    );
+import catchAsync from '../../utils/catchAsync';
+const createStudent = catchAsync(async (req, res, next) => {
+  const { student: studentData, password } = req.body;
+  const result = await StudentServices.createStudentIntoDB(
+    password,
+    studentData,
+  );
 
-    sendResponse(res, {
-      statusCode: status.OK,
-      success: true,
-      message: 'Student is created successfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: 'Student is created successfully',
+    data: result,
+  });
+});
 
 export const UserControllers = {
   createStudent,
